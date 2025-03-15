e31415e9ece82eb6f2eada8542913f7a5a924ccb
anshu1994 <anshumanbhowmick6@gmail.com>
2025-03-15 13:02:21 +0530

    adding search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import "../../styles.css";
 import { headerLogoURL } from "../utils/consts";
 
 const Header = ()=>{
+    const [buttonUI,setButtonUI] = useState("Login")
     return (
         <div className="header">
             <a href="https://github.com/"><img className="header-logo" src={headerLogoURL}></img></a>
@@ -11,6 +13,7 @@ const Header = ()=>{
                     <li>All Users</li>
                     <li>About us</li>
                     <li>Contact us</li>
+                    <button onClick={()=>buttonUI==="Login" ? setButtonUI("Logout"): setButtonUI("Login")}>{buttonUI}</button>
                 </ul>
         </div>
     )
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,17 @@
+import "../../styles.css"
+
+const Shimmer = ()=>{
+    return(
+        <div className="shimmer-container">
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+            <div className="shimmer-card"/>
+        </div>
+    )
+}
+export default Shimmer;
